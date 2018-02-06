86a3363a9a5e2268d2d0df0898f5e6cbae24ea3e
Vladimir Beletskiy <shinzironine@gmail.com>
2018-02-06 11:01:37 +0500

    Coding challenge #1. Fractal Tree

---
+++ CodingChallenge/Assets/CodingChallenge/FractalTree/FractalTree.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodingChallenge.FractalTree
+{
+    public class FractalTree : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject branchPrefab;
+        
+        [SerializeField]
+        private float initialBranchLength;            
+        
+        [SerializeField]
+        private float angle;
+        
+        [SerializeField]
+        private float depth;
+        
+        [SerializeField]
+        private float updateSpeed;
+
+        public GameObject BranchPrefab
+        {
+            get { return branchPrefab; }
+            set { branchPrefab = value; }
+        }
+
+        public float InitialBranchLength
+        {
+            get { return initialBranchLength; }
+        }
+
+        public float Angle
+        {
+            get { return angle; }
+        }
+
+        private List<FractalTreeBranch> tree = new List<FractalTreeBranch>();
+        
+        void Awake()
+        {
+            Vector3 initialEnd = transform.position + new Vector3(0, initialBranchLength, 0);
+            var branch = InstantiateBranch(transform.position, initialEnd);
+            tree.Add(branch);
+            
+            branch.Branch(this, depth);
+        }
+
+        private FractalTreeBranch InstantiateBranch(Vector3 branchStart, Vector3 branchEnd)
+        {
+            GameObject branch = Instantiate(branchPrefab, transform);
+            var fractalTreeBranch = branch.GetComponent<FractalTreeBranch>();
+            fractalTreeBranch.DrawLine(branchStart, branchEnd);
+            return fractalTreeBranch;
+        }
+
+        public void RedrawChildren()
+        {
+            angle = FindObjectOfType<Slider>().value;
+            if (tree[0] != null)
+            {
+                tree[0].RedrawChildren();
+            }
+        }
+
+        void Update()
+        {
+            var slider = FindObjectOfType<Slider>();
+            slider.value += updateSpeed * Time.deltaTime;
+            RedrawChildren();
+
+            if (slider.value >= slider.maxValue)
+            {
+                slider.value = slider.minValue;
+            }
+        }
+
+
+    }
+}
--- CodingChallenge/Assets/CodingChallenge/FractalTree/FractalTreeBranch.cs
+++ CodingChallenge/Assets/CodingChallenge/FractalTree/FractalTreeBranch.cs
@@ -4,32 +4,81 @@ namespace CodingChallenge.FractalTree
 {
     public class FractalTreeBranch : MonoBehaviour
     {
+        private static int count;
+        
         [SerializeField]
         private LineRenderer lineRenderer;
+        
+        private FractalTree root;
 
         private Vector3 branchStart;
         private Vector3 branchEnd;
 
-        public void DrawLine(Vector3 branchStart, Vector3 branchEnd)
+        private FractalTreeBranch right;
+        private FractalTreeBranch left;
+
+        public Vector3 BranchStart
         {
-            this.branchStart = branchStart;
-            this.branchEnd = branchEnd;
-            
-            lineRenderer.positionCount = 2;
-            lineRenderer.SetPosition(0, branchStart);
-            lineRenderer.SetPosition(1, branchEnd);
+            get { return branchStart; }
+            set
+            {
+                branchStart = value; 
+                lineRenderer.SetPosition(0, branchStart);
+            }
         }
 
-        public void Branch()
+        public Vector3 BranchEnd
         {
-            Vector3 direction = branchEnd - branchStart;
-            Vector3 directionRight = RotateVector(direction, Mathf.PI / 8);
-            Vector3 directionLeft = RotateVector(direction, -Mathf.PI / 8);
-            
-            FractalTreeBranch right = InstantiateBranch(branchEnd, directionRight + branchEnd);
-            FractalTreeBranch left = InstantiateBranch(branchEnd, directionLeft + branchEnd);
-            
+            get { return branchEnd; }
+            set
+            {
+                branchEnd = value; 
+                lineRenderer.SetPosition(1, branchEnd);
+                RedrawChildren();
+            }
+        }
 
+        private void Awake()
+        {
+            lineRenderer.positionCount = 2;
+        }
+
+        public void DrawLine(Vector3 branchStart, Vector3 branchEnd)
+        {
+            BranchStart = branchStart;
+            BranchEnd = branchEnd;
+        }
+
+        public void RedrawChildren()
+        {
+            if (right != null && left != null)
+            {
+                right.BranchStart = BranchEnd;
+                left.BranchStart = BranchEnd;
+                right.BranchEnd = CalculateChildEndVector(false);
+                left.BranchEnd = CalculateChildEndVector(true);
+            }
+        }
+
+        private Vector3 CalculateChildEndVector(bool leftTurn)
+        {
+            Vector3 direction = BranchEnd - BranchStart;
+            int coeff = leftTurn ? -1 : 1;
+            Vector3 rotatedDirection = RotateVector(direction, coeff * root.Angle) * root.InitialBranchLength;
+            return rotatedDirection + BranchEnd;
+        }
+
+        public void Branch(FractalTree root, float depth)
+        {
+            this.root = root;
+
+            if (depth > 0)
+            {
+                right = InstantiateBranch(branchEnd, CalculateChildEndVector(false));
+                right.Branch(root, depth - 1);
+                left = InstantiateBranch(branchEnd, CalculateChildEndVector(true));
+                left.Branch(root, depth - 1);                
+            }
         }
 
         private Vector3 RotateVector(Vector3 vector, float angle)
@@ -38,15 +87,21 @@ namespace CodingChallenge.FractalTree
             var cos = Mathf.Cos(angle);
             var sin = Mathf.Sin(angle);
             
-            //WTF
             rotatedVector.x = vector.x * cos - vector.y * sin;
-            rotatedVector.y = vector.y * sin + vector.x * cos;
+            rotatedVector.y = vector.x * sin + vector.y * cos;
             return rotatedVector;
         }
         
-        private FractalTreeBranch InstantiateBranch(Vector3 start, Vector3 end)
+        public FractalTreeBranch InstantiateBranch(Vector3 start, Vector3 end)
         {
-            GameObject branch = Instantiate(gameObject, transform);
+             return InstantiateBranch(start, end, transform);
+        }
+        
+        public FractalTreeBranch InstantiateBranch(Vector3 start, Vector3 end, Transform parent)
+        {
+            count++;
+            GameObject branch = Instantiate(root.BranchPrefab, parent);
+            branch.name = count.ToString();
             var fractalTreeBranch = branch.GetComponent<FractalTreeBranch>();
             fractalTreeBranch.DrawLine(start, end);
             return fractalTreeBranch;
