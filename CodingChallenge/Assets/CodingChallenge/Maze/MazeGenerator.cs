d1512280521e8381222fc5ab11ac8f5895cd6b61
Vladimir Beletskiy <shinzironine@gmail.com>
2018-02-21 19:28:53 +0500

    Maze generator wip

--- CodingChallenge/Assets/CodingChallenge/Maze/MazeGenerator.cs
+++ CodingChallenge/Assets/CodingChallenge/Maze/MazeGenerator.cs
@@ -1,42 +1,64 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace CodingChallenge.Maze
 {
     public class MazeGenerator : MonoBehaviour
     {
+        private readonly List<int> neighbours = new List<int>();
+
         [SerializeField]
         private int cols;
 
         [SerializeField]
         private int rows;
-        
+
         [SerializeField]
         private float xOffset;
-        
+
         [SerializeField]
         private float yOffset;
-        
+
         [SerializeField]
         private GameObject mazeCellPrefab;
 
         private MazeCell[] mazeCells;
         private bool[] visitedCells;
 
-        private MazeCell currentCell;
+        private int currentCellId;
 
         void Start()
         {
             CreateMaze();
             OffsetToFitScreen();
 
-            SetVisited(0);
+            currentCellId = 0;
+            SetVisited(currentCellId);
         }
-        
+
+        void Update()
+        {
+            MoveNextNeighbour();
+        }
+
+        private void MoveNextNeighbour()
+        {
+            var neigbourId = PickNeighbour(currentCellId);
+
+            if (neigbourId != -1)
+            {
+                SetVisited(neigbourId);
+                currentCellId = neigbourId;
+            }
+        }
+
         private void SetVisited(int id)
         {
-            currentCell = mazeCells[id];
+            MazeCell cell = mazeCells[id];
             visitedCells[id] = true;
-            currentCell.ChangeColor(Color.blue);
+            cell.ChangeColor(Color.blue);
         }
 
         private void OffsetToFitScreen()
@@ -48,9 +70,34 @@ namespace CodingChallenge.Maze
             transform.position = transformPosition;
         }
 
+        private int PickNeighbour(int id)
+        {
+            neighbours.Clear();
+
+            FillNeighbour(MazeUtil.LeftCellId(id, rows, cols));
+            FillNeighbour(MazeUtil.RightCellId(id, rows, cols));
+            FillNeighbour(MazeUtil.TopCellId(id, rows, cols));
+            FillNeighbour(MazeUtil.BottomCellId(id, rows, cols));
+
+            if (neighbours.Count > 0)
+            {
+                return neighbours[Random.Range(0, neighbours.Count)];
+            }
+
+            return -1;
+        }
+
+        private void FillNeighbour(int cellId)
+        {
+            if (cellId != -1 && visitedCells[cellId] == false)
+            {
+                neighbours.Add(cellId);
+            }
+        }
+
         private void CreateMaze()
         {
-            var numberCells = cols + rows;
+            var numberCells = cols * rows;
             mazeCells = new MazeCell[numberCells];
             visitedCells = new bool[numberCells];
 
@@ -60,7 +107,7 @@ namespace CodingChallenge.Maze
                 {
                     var cellInstance = Instantiate(mazeCellPrefab);
                     var mazeCell = cellInstance.GetComponent<MazeCell>();
-                    mazeCells[i + j] = mazeCell;
+                    mazeCells[i * cols + j] = mazeCell;
 
                     Vector3 pos = Vector3.zero;
                     pos.y = i * (1 + yOffset);
---
+++ CodingChallenge/Assets/CodingChallenge/Maze/MazeUtil.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CodingChallenge.Maze
+{
+    public static class MazeUtil
+    {
+        public static int LeftCellId(int id, int rows, int cols)
+        {
+            if (id % cols == 0)
+            {
+                return -1;
+            }
+            
+            var leftCellId = id - 1;
+            return leftCellId;
+        }
+        
+        public static int RightCellId(int id, int rows, int cols)
+        {
+            if (id % cols == cols - 1 || id + 1 >= rows * cols)
+            {
+                return -1;
+            }
+            
+            var cellId = id + 1;
+            return cellId;
+        }
+        
+        public static int TopCellId(int id, int rows, int cols)
+        {
+            var cellId = id + cols;
+
+            if (cellId >= rows * cols)
+            {
+                return -1;
+            }
+            
+            return cellId;
+        }
+        
+        public static int BottomCellId(int id, int rows, int cols)
+        {
+            var cellId = id - cols;
+
+            if (cellId < 0)
+            {
+                return -1;
+            }
+            
+            return cellId;
+        }
+
+        public static int GetRowId(int id, int cols)
+        {
+            return (int) Math.Floor((double) (id / cols));
+        }
+        
+        public static int GetColId(int id, int cols)
+        {
+            return id % cols;
+        }
+
+    }
+}
